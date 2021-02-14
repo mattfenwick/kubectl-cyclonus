06dd5e3e6454caaeacf752b6faa1688f4147e746
Matthew Fenwick <mfenwick100@gmail.com>
2021-02-14 08:46:09 -0500

    handle code review comments

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/mattfenwick/kubectl-cyclonus/pkg/cli"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // required for GKE
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 func main() {
